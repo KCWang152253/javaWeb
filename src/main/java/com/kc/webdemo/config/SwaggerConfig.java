5495c998be7a16a76c022df3befdf7e72c185fbc
2747962529@qq.com <2747962529@qq.com>
2023-06-24 23:27:29 +0800

    20230624  启用Swagger

--- src/main/java/com/kc/webdemo/config/SwaggerConfig.java
+++ src/main/java/com/kc/webdemo/config/SwaggerConfig.java
@@ -22,6 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
