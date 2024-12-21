a4da9aac8a65bdb32560a54c48f8fa2faf9d4122
2747962529@qq.com <2747962529@qq.com>
2024-12-21 22:19:48 +0800

    springboot 集成 dubbo

--- dubbo-admin-develop/dubbo-admin-server/src/main/java/org/apache/dubbo/admin/DubboAdminApplication.java
+++ dubbo-admin-develop/dubbo-admin-server/src/main/java/org/apache/dubbo/admin/DubboAdminApplication.java
@@ -22,15 +22,38 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication(exclude={
-		HibernateJpaAutoConfiguration.class
+import java.io.File;
+
+@SpringBootApplication(exclude = {
+        HibernateJpaAutoConfiguration.class
 })
 @EnableDubbo(scanBasePackages = {"org.apache.dubbo.admin.provider"})
 @MapperScan(basePackages = {"org.apache.dubbo.admin.mapper"})
 public class DubboAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DubboAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+//		SpringApplication.run(DubboAdminApplication.class, args);
+
+
+        // 创建SpringApplication实例
+        SpringApplication application = new SpringApplication(DubboAdminApplication.class);
+        // 添加自定义的ApplicationContextInitializer
+        application.addInitializers(context -> {
+            // 获取Environment对象
+            Environment env = context.getEnvironment();
+            // 从Environment中读取"spring.application.name"属性值
+            String appName = env.getProperty("spring.application.name");
+            String filePath = System.getProperty("user.home") + File.separator + ".dubbo" + File.separator + appName;
+            // 修改dubbo的本地缓存路径，避免缓存冲突
+            System.setProperty("dubbo.meta.cache.filePath", filePath);
+            System.setProperty("dubbo.mapping.cache.filePath", filePath);
+        });
+        //启动应用
+        application.run(args);
+
+
+    }
 }
