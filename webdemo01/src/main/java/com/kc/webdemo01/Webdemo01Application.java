e17048a92f7dbcfd65ba74cb8c35a6538c75824e
2747962529@qq.com <2747962529@qq.com>
2024-12-24 00:11:14 +0800

    springboot 集成 dubbo

--- webdemo01/src/main/java/com/kc/webdemo01/Webdemo01Application.java
+++ webdemo01/src/main/java/com/kc/webdemo01/Webdemo01Application.java
@@ -1,7 +1,11 @@
 package com.kc.webdemo01;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,5 +15,14 @@ public class Webdemo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Webdemo01Application.class, args);
 	}
+
+
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName){
+		return (registry) -> registry.config().commonTags("application",applicationName);
+	}
+
 
 }
